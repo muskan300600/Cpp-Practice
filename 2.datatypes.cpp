55a75aab9abf2c354c49d1fe55e7a1049faf3f1c
Muskan Jha <80477586+muskan300600@users.noreply.github.com>
2021-08-30 14:41:26 +0530

    Datatypes

---
+++ 2.datatypes.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a;
+    a=1;
+    cout<<a<<endl;
+
+    float b =78.9;
+    cout<<b<<endl;
+
+    cout<<"The value of a is "<<a<<ends<<" and that of b is"<<b<<endl;
+
+    int const c =10;
+    cout<<c<<endl;
+
+    int d=9;
+    long e=10;
+    long long f=90;
+
+}
