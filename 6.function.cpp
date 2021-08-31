1a22991eac14d8aaf2ce8594965cd6cbc714230e
Muskan Jha <80477586+muskan300600@users.noreply.github.com>
2021-08-31 19:03:09 +0530

    Functions

---
+++ 6.function.cpp
@@ -0,0 +1,15 @@
+# include <iostream>
+using namespace std;
+
+int sum(int a,int b){
+    return a+b;
+}
+
+int main(){
+int first,second;
+    cout<<"enter the first number: ";
+    cin>>first;
+    cout<<"enter the second number: ";
+    cin>>second;
+    cout<<"The sum of both the numbers is: "<<sum(first,second);
+}
