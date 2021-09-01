5d4407fd05de662686a932fc2fef65acb34cc227
Muskan Jha <80477586+muskan300600@users.noreply.github.com>
2021-09-01 20:02:10 +0530

    About datatypes

--- 2.datatypes.cpp
+++ 2.datatypes.cpp
@@ -18,4 +18,12 @@ int main(){
     long e=10;
     long long f=90;
 
+    float d = 34.4F;
+    long double e = 34.4L;
+    cout << "The size of 34.4 is " << sizeof(34.4) << endl;
+    cout << "The size of 34.4f is " << sizeof(34.4f) << endl;
+    cout << "The size of 34.4F is " << sizeof(34.4F) << endl;
+    cout << "The size of 34.4l is " << sizeof(34.4l) << endl;
+    cout << "The size of 34.4l is " << sizeof(34.4L) << endl;
+
 }
