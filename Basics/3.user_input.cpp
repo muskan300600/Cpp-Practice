02d106ee645f0e6958402334bef1baed8cbb198f
Muskan Jha <80477586+muskan300600@users.noreply.github.com>
2021-09-20 20:41:20 +0530

    Add files via upload

---
+++ Basics/3.user_input.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a,b;
+    cout<<"enter the first number: ";
+    cin>>a;
+    cout<<"enter the second number: ";
+    cin>>b;
+
+    cout<<"The sum is: "<<a+b<<endl;
+    cout<<(float)a/b;
+}
