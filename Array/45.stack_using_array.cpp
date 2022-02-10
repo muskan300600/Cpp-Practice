00d88cf1ce896ba1e4c17dc5219eaf9eff818236
Muskan Jha <80477586+muskan300600@users.noreply.github.com>
2022-02-10 19:07:41 +0530

    Add files via upload

---
+++ Array/45.stack_using_array.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class MyStack{
+private:
+    int arr[1000];
+    int top;
+public:
+    MyStack(){
+        top=-1;
+    }
+    int pop();
+    void push(int);
+};
+
+void MyStack :: push(int x){
+    arr[++top]=x;
+}
+
+int MyStack :: pop(){
+    if(top!=-1){
+        return arr[top--];
+    }
+    else{
+        return -1;
+    }
+}
+
+int main(){
+    
+}
