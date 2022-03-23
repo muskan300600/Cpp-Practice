947eb849c0ecefd3132799cd673d53f8d92b11f1
Muskan Jha <80477586+muskan300600@users.noreply.github.com>
2022-03-23 22:50:26 +0530

    Add files via upload

---
+++ Strings/9.substring.cpp
@@ -0,0 +1,13 @@
+#include <algorithm>
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    string S = "cdbkdub";
+    int L=0;
+    int R=5;
+    for(int i=L;i<=R;i++){
+        cout<<S[i];
+    }
+}
