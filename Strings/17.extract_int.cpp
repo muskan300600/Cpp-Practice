241622e0c115f7017832222b6d92cfda9b001f03
Muskan Jha <80477586+muskan300600@users.noreply.github.com>
2022-03-29 23:20:43 +0530

    Add files via upload

---
+++ Strings/17.extract_int.cpp
@@ -0,0 +1,10 @@
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+int main(){
+    string s = "1: Prakhar Agrawal, 2: Manish Kumar Rai, 3: Rishabh Gupta56"
+    
+}
