8a9f226c0d6959031c80f228b282694d670668f9
Muskan Jha <80477586+muskan300600@users.noreply.github.com>
2022-05-27 23:04:38 +0530

    Add files via upload

---
+++ Strings/39.remainder.cpp
@@ -0,0 +1,15 @@
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+int main(){
+    string s = "8";
+    int rem =0;
+    for(int i=0;i<s.length();i++){
+        rem = rem * 10+(s[i]-'0');
+        rem = rem%7;
+    }  
+    cout<<rem;    
+}
